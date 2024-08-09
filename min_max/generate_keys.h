c559ff7977a058745f07277643c7bd233f523f3d
Ajith Suresh <135689928+ajithsureshtii@users.noreply.github.com>
2024-08-09 09:17:49 +0400

    Merging mi_max template to development (#4)

---
+++ min_max/generate_keys.h
@@ -0,0 +1,25 @@
+#include "openfhe.h"
+#include "stdio.h"
+#include <cmath>
+#include <string>
+#include <vector>
+
+// header files needed for serialization
+#include "ciphertext-ser.h"
+#include "cryptocontext-ser.h"
+#include "key/key-ser.h"
+#include "scheme/ckksrns/ckksrns-ser.h"
+
+using namespace lbcrypto;
+using namespace std;
+
+// Key location
+static const string pubKeyLocation = "../files/pub.bin";
+static const string privKeyLocation = "../files/priv.bin";
+static const string multKeyLocation = "../files/mult.bin";
+static const string rotKeyLocation = "../files/rot.bin";
+static const string ccLocation = "../files/cc.bin";
+static const string inputLocation = "../files/in.bin";
+static const string outputLocation = "../files/out.bin";
+
+
---
+++ min_max/max_min_ckks.h
@@ -0,0 +1,49 @@
+#include "openfhe.h"
+#include "stdio.h"
+#include <cmath>
+#include <string>
+#include <vector>
+
+// header files needed for serialization
+#include "ciphertext-ser.h"
+#include "cryptocontext-ser.h"
+#include "key/key-ser.h"
+#include "scheme/ckksrns/ckksrns-ser.h"
+
+using namespace lbcrypto;
+using namespace std;
+
+class MaxMinCKKS {
+    CryptoContext<DCRTPoly> m_cc;
+    PublicKey<DCRTPoly> m_PublicKey;
+    Ciphertext<DCRTPoly> m_InputC;
+    Ciphertext<DCRTPoly> m_OutputC;
+    Plaintext m_MaskLookup;
+    Plaintext m_Half;
+    Plaintext m_One;
+    string m_PubKeyLocation;
+    string m_MultKeyLocation;
+    string m_RotKeyLocation;
+    string m_CCLocation;
+    string m_InputLocation;
+    string m_OutputLocation;
+    int array_limit;
+
+public:
+    MaxMinCKKS(string ccLocation, string pubKeyLocation, string multKeyLocation,
+            string rotKeyLocation, string inputLocation, string outputLocation);
+
+    void initCC();
+    void eval();
+    void deserializeOutput();
+
+    Ciphertext<DCRTPoly> sign(Ciphertext<DCRTPoly> m_InputC);
+    Ciphertext<DCRTPoly> cond_swap(Ciphertext<DCRTPoly> m_InputC, bool is_even);
+
+    Ciphertext<DCRTPoly> compare(Ciphertext<DCRTPoly> m_InputA, Ciphertext<DCRTPoly> m_InputB);
+    Ciphertext<DCRTPoly> round(Ciphertext<DCRTPoly> m_InputC, int len_comparison_vector, bool is_max);
+
+    vector<double> ChebyshevCoefficientsSign(int degree, double a, double b);
+    Ciphertext<DCRTPoly> compare_test(Ciphertext<DCRTPoly> m_InputA, Ciphertext<DCRTPoly> m_InputB);
+    void eval_test();
+};
